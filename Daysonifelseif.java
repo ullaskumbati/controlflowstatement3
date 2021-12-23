0403a2d653aeb84f48d5c8039a619e124d184d02
ullaskumbati <ullasbk.xworkz@gmail.com>
2021-12-23 21:32:04 +0530

    added programme that prints days using if else if statement

---
+++ Daysonifelseif.java
@@ -0,0 +1,30 @@
+class Daysonifelseif{
+	public static void main (String [] args){
+		byte days =8;
+		if (days==1){
+			System.out.println("Sunday");
+		}
+		else if(days==2){
+			System.out.println("Monday");
+		}
+		else if(days==3){
+			System.out.println("Tuesday");
+		}
+		else if (days==4){
+			System.out.println("Wednesday");
+		}
+		else if(days==5){
+			System.out.println("Thursday");
+		}
+		else if (days==6){
+			System.out.println("friday");
+		}
+		else if (days==7){
+			System.out.println("Saturday");
+		}
+		else {
+			System.out.println("Dont act oversmart we have only 7 days in a week");
+		}
+		
+	}
+}
