bad122a9fb83843ea5c756be4ef8a61e6d40ee6b
ullaskumbati <ullasbk.xworkz@gmail.com>
2021-12-23 21:35:49 +0530

    added programme that prints months using if statement

---
+++ Monthsonif.java
@@ -0,0 +1,49 @@
+class Monthsonif{
+	public static void main (String []args){
+		byte month=12;
+		if (month==1){
+			System.out.println("January");
+		}
+
+		if (month==2){
+			System.out.println("February");
+		}
+		
+		if(month==3){
+			System.out.println("March");
+		}
+
+		if(month==4){
+			System.out.println("April");
+		}
+		
+		if(month==5){
+			System.out.println("may");
+		}
+		
+		if(month==6){
+			System.out.println("June");
+		}
+
+		if(month==7){
+			System.out.println("July");
+		}
+		if(month==8){
+			System.out.println("August");
+		}
+		if(month==9){
+			System.out.println("September");
+		}
+		if(month==10){
+			System.out.println("October");
+		}
+		if(month==11){
+			System.out.println("November");
+		}
+		if(month==12){
+			System.out.println("December");
+		}
+
+
+	}
+}
